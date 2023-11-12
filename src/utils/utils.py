f1c9d7d93bfc7e42f60c6197adc6d83a84148b3e
matheusssilva991 <matheus.ssilva991@gmail.com>
2023-11-12 09:41:55 -0300

    fix: environment, models, metadatas

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -14,6 +14,8 @@ from pydicom.sequence import Sequence
 from pydicom.valuerep import PersonName
 from copy import deepcopy
 from skimage.feature import graycomatrix, graycoprops
+from skimage.draw import polygon
+import plistlib
 
 
 def load_json(object_name: str, path=None) -> None | object:
@@ -381,3 +383,34 @@ def get_glcm_features(image, distances, angles, levels, symmetric, normed, prope
                 glcm_features.append(kurtosis) """
 
     return glcm_features
+
+
+def load_inbreast_mask(mask_path, imshape=(4084, 3328)):
+    """
+    This function loads a osirix xml region as a binary numpy array for INBREAST
+    dataset
+    @mask_path : Path to the xml file
+    @imshape : The shape of the image as an array e.g. [4084, 3328]
+    return: numpy array where positions in the roi are assigned a value of 1.
+    """
+
+    mask = np.zeros(imshape)
+    with open(mask_path, 'rb') as mask_file:
+        plist_dict = plistlib.load(mask_file, fmt=plistlib.FMT_XML)['Images'][0]
+        numRois = plist_dict['NumberOfROIs']
+        rois = plist_dict['ROIs']
+        assert len(rois) == numRois
+        for roi in rois:
+            numPoints = roi['NumberOfPoints']
+            points = roi['Point_px']
+            assert numPoints == len(points)
+            points = [eval(point) for point in points]
+            if len(points) <= 2:
+                for point in points:
+                    mask[int(point[1]), int(point[0])] = 1
+            else:
+                x, y = zip(*points)
+                col, row = np.array(x), np.array(y) ##x coord is the column coord in an image and y is the row
+                poly_x, poly_y = polygon(row, col, shape=imshape)
+                mask[poly_x, poly_y] = 1
+    return mask
