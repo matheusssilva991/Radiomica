21be808c9ff922f710f815efdfc8957dbc1cf2d7
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-05-26 20:54:31 -0300

    feat: testes roi cropped centralizada

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -333,3 +333,31 @@ def extract_roi(image: np.array, mask: np.array) -> np.array:
     result[mask == 0] = 0
 
     return result
+
+
+def plot_history(history):
+    """Plots accuracy/loss for training/validation set as a function of the
+       epochs
+
+        :param history: Training history of model
+        :return:
+    """
+
+    _, axs = plt.subplots(1, 2, figsize=(10, 4))
+
+    # create accuracy sublpot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="valid accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # create error sublpot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="valid error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
